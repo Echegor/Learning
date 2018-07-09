72e3b10a06d126add336a5833727cb440367433d
Luis Echegorri <lme4@njit.edu>
2018-07-09 17:48:02 -0400

    * k

--- src/com/archelo/string/PermutationOfPalindrome.java
+++ src/com/archelo/string/PermutationOfPalindrome.java
@@ -2,6 +2,14 @@ package com.archelo.string;
 
 import java.util.HashMap;
 
+/*
+* Palindrome Permutation: Given a string, write a function to check if it is a permutation of a palindrome.
+* A palindrome is a word or phrase that is the same forwards and backwards. A permutation
+* is a rearrangement of letters. The palindrome does not need to be limited to just dictionary words.
+* EXAMPLE
+* Input: Tact Coa
+* Output: True (permutations: "taco cat". "atco cta". etc.)
+* */
 public class PermutationOfPalindrome {
     public static void main(String[] args) {
         String test1 = "taco cat";
