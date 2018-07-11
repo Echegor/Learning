b1e67b5e2b15f446ac6f13c2be4f50d869b09a63
Luis Echegorri <lme4@njit.edu>
2018-07-11 13:48:36 -0400

    * matrix rotation finished

--- src/com/archelo/string/RotateMatrix.java
+++ src/com/archelo/string/RotateMatrix.java
@@ -4,13 +4,13 @@ import javax.sound.midi.Soundbank;
 import java.awt.*;
 
 /*
-* 1.7 Rotate Matrix: Given an image represented by an NxN matrix, where each pixel in the image is 4
-* bytes, write a method to rotate the image by 90 degrees. can you do this in place?
-* */
+ * 1.7 Rotate Matrix: Given an image represented by an NxN matrix, where each pixel in the image is 4
+ * bytes, write a method to rotate the image by 90 degrees. can you do this in place?
+ * */
 public class RotateMatrix {
     public static final int pixelSize = 4;
-    public static final int imageWidth = 5;
-    public static final int imageHeight = 5;
+    public static final int imageWidth = 3;
+    public static final int imageHeight = 3;
 
     public static void main(String[] args) {
         Pixel[][] image = new Pixel[imageWidth][imageHeight];
@@ -22,63 +22,81 @@ public class RotateMatrix {
 
     }
 
-    public static void initArray(Pixel[][] input){
+    public static void initArray(Pixel[][] input) {
         int count = 0;
-        for(int i =0 ; i < input.length ; i++){
-            for(int j = 0 ; j < input[i].length ; j++){
-                input[i][j] = new Pixel(i+j);
+        for (int i = 0; i < input.length; i++) {
+            for (int j = 0; j < input[i].length; j++) {
+                input[i][j] = new Pixel(count++);
             }
         }
     }
 
-    public static void printArray(Pixel[][] input){
-        for(int i =0 ; i < input.length ; i++){
-            for(int j = 0 ; j < input[i].length ; j++){
-                System.out.print(input[i][j].pixel + " ");
+    public static void printArray(Pixel[][] input) {
+        for (int i = 0; i < input.length; i++) {
+            for (int j = 0; j < input[i].length; j++) {
+                System.out.printf("%02d ",input[i][j].pixel);
+//                System.out.print( + " ");
             }
             System.out.println();
         }
     }
 
     /*
-    * Rotates an image 90 degrees to the right
-    * */
-    public static Pixel[][] rotateImage90(Pixel[][] input){
-        Point topLeft = new Point(0,0);
-        Point topRight = new Point(input.length,0);
-        Point bottomRight = new Point(input.length,input.length);
-        Point bottomLeft= new Point(0,input.length);
+     * Rotates an image 90 degrees to the right
+     * */
+    public static Pixel[][] rotateImage90(Pixel[][] input) {
         int currentSquareSize = input.length - 1;
+        Point topLeft = new Point(0, 0);
+        Point topRight = new Point(currentSquareSize, 0);
+        Point bottomRight = new Point(currentSquareSize, currentSquareSize);
+        Point bottomLeft = new Point(0, currentSquareSize);
+
 
         Pixel temp;
-        while(topLeft.x < topRight.x){
-            //System.out.println("swapping");
-            //save top left
-            temp = input[topRight.x][topRight.y];
+        while (currentSquareSize > 1) {
+
+            //save top right
+            System.out.println("Saving top right " + topRight);
+            System.out.println("Saving topRight = " + topRight + " " + input[topRight.y][topRight.x].pixel);
+            temp = input[topRight.y][topRight.x];
 
             //put topLeft in topRight
-            input[topRight.x][topRight.y] = input[topLeft.x][topLeft.y];
+            System.out.println("topRight = " + topRight + " Moving " + input[topLeft.y][topLeft.x].pixel + " to " + input[topRight.y][topRight.x].pixel);
+            input[topRight.y][topRight.x] = input[topLeft.y][topLeft.x];
 
             //put bottomLeft in topLeft
-            input[topLeft.x][topLeft.y] = input[bottomLeft.x][bottomLeft.y];
+            System.out.println("topLeft = " + topLeft + " Moving " + input[bottomLeft.y][bottomLeft.x].pixel + " to " + input[topLeft.y][topLeft.x].pixel);
+            input[topLeft.y][topLeft.x] = input[bottomLeft.y][bottomLeft.x];
 
             //put bottomRight in bottomLeft
-            input[bottomLeft.x][bottomLeft.y] = input[bottomRight.x][bottomRight.y];
+            System.out.println("bottomLeft = " + bottomLeft + " Moving " + input[bottomRight.y][bottomRight.x].pixel + " to " + input[bottomLeft.y][bottomLeft.x].pixel);
+            input[bottomLeft.y][bottomLeft.x] = input[bottomRight.y][bottomRight.x];
 
             //put topRight in bottomRight
-            input[topLeft.x][topLeft.y] = temp;
+            System.out.println("bottomRight = " + bottomRight + "Moving " + temp.pixel + " to " + input[bottomRight.y][bottomRight.x].pixel);
+            input[bottomRight.y][bottomRight.x] = temp;
 
+            printArray(input);
+            System.out.println("topleft.x " + topLeft.x+ " currentsquare size " + currentSquareSize);
             /*
-            * At this point, we can no longer swap pieces and must move onto the next inner square
-            * */
-            if(topLeft.x == currentSquareSize){
+             * At this point, we can no longer swap pieces and must move onto the next inner square
+             * */
+            if (topLeft.x >= currentSquareSize-1) {
+                System.out.println("Resetting");
                 currentSquareSize--;
-                topLeft.x = topLeft.y++ + 1;
-                topRight.y = topRight.x-- + 1;
-                bottomRight.x = bottomRight.y-- -1;
-                bottomLeft.y = bottomLeft.x-- - 1;
-            }
-            else{
+                topLeft.x = topLeft.y + 1;
+                topLeft.y = topLeft.x;
+
+                topRight.x = currentSquareSize;
+                topRight.y = topLeft.x;
+
+                bottomRight.x = currentSquareSize;
+                bottomRight.y = currentSquareSize;
+
+                bottomLeft.x = bottomLeft.x + 1;
+                bottomLeft.y = currentSquareSize;
+            } else {
+                System.out.println("Incrementing");
                 topLeft.x++;
                 topRight.y++;
                 bottomRight.x--;
@@ -86,26 +104,19 @@ public class RotateMatrix {
             }
         }
 
+
         return input;
     }
-    public static void swap(Pixel a,Pixel b){
-        Pixel temp = a;
-        a = b;
-        b = temp;
-    }
 
-    public static void swap(){
-
-    }
-
-    public static class Pixel{
+    public static class Pixel {
 //        public byte[] pixel;
 //        public Pixel(){
 //            pixel = new byte[pixelSize];
 //        }
 
         public int pixel;
-        public Pixel(int pixel){
+
+        public Pixel(int pixel) {
             this.pixel = pixel;
         }
 
